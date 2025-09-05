fe5545c0e35054e8cce6b9a2553bfbea92a3a213
Adam <adamcolyer@gmail.com>
2025-09-05 16:24:48 -0500

    goo

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode, useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App.jsx';
 import { ToastProvider } from './components/ToastContext.jsx';
