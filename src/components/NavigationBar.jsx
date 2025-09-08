fd9f960a0232584f80625453966ab87d95437d6a
Adam <adamcolyer@gmail.com>
2025-09-07 23:03:29 -0500

    plz work

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -4,13 +4,13 @@ import { LinkContainer } from 'react-router-bootstrap';
 const NavigationBar = () => (
   <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect sticky="top">
     <Container>
-      <LinkContainer to="/">
+      <LinkContainer to="/fakestore-app/">
         <Navbar.Brand>Sample Ecommerce Store</Navbar.Brand>
       </LinkContainer>
       <Navbar.Toggle aria-controls="main-navbar-nav" />
       <Navbar.Collapse id="main-navbar-nav">
         <Nav className="ms-auto">
-          <LinkContainer to="/">
+          <LinkContainer to="/fakestore-app/">
             <Nav.Link>Home</Nav.Link>
           </LinkContainer>
           <LinkContainer to="/products">
